41379a3a341a07e7d7cf4a242849044ae1aff2c2
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-01-08 10:39:06 +0530

    first flutter project

---
+++ lib/homepage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController searchController = new TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ARNE NEWS"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            //Search Wala Container
+
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24)),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if ((searchController.text).replaceAll(" ", "") ==
+                        "") {
+                      print("Blank search");
+                    } else {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                    }
+                  },
+                  child: Container(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.blueAccent,
+                    ),
+                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,  // key bord me search wala icon aa jayega
+                    onSubmitted: (value){
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Health"),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
